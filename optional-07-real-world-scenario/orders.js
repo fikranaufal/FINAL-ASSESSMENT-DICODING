ea2e129eed7f1e9e4675026f5131140ac7a8d78e
fikranaufal <fikran.naufal@gmail.com>
2025-07-28 11:42:52 +0700

    Task 7 is clear

--- optional-07-real-world-scenario/orders.js
+++ optional-07-real-world-scenario/orders.js
@@ -12,7 +12,7 @@ function addOrder(customerName, items) {
   let totalPrice = 0;
   for (let i = 0; i < items.length; i++) {
     totalPrice = totalPrice + items[i].price;
-  }
+  };
 
   orders.push({
     id : generateUniqueId(),
@@ -25,19 +25,38 @@ function addOrder(customerName, items) {
 
 // TODO: selesaikan fungsi updateOrderStatus
 function updateOrderStatus(orderId, status) {
-  const index = 0;
 
   for (let i = 0; i < orders.length; i++) {
     if (orders[i].id === orderId) {
       return orders[i].status = status;
-    }
-  }
+    };
+  };
 }
 
 // TODO: selesaikan fungsi calculateTotalRevenue dari order yang berstatus Selesai
-function calculateTotalRevenue() {}
+function calculateTotalRevenue() {
+  const indexOrderisDone = [];
+  let totalRevenue = 0;
+
+  for (let i = 0; i < orders.length; i++) {
+    if (orders[i].status == 'Selesai')
+      indexOrderisDone.push(i);
+  };
+
+  for (const j of indexOrderisDone) {
+    totalRevenue = totalRevenue + orders[j].totalPrice;
+  };
+
+  return totalRevenue;
+}
 
 // TODO: selesaikan fungsi deleteOrder
-function deleteOrder(id) {}
+function deleteOrder(id) {
+  for (let i = 0; i < orders.length; i++) {
+    if (orders[i].id === id) {
+      return orders.splice(i,1);
+    };
+  };
+}
 
 export { orders, addOrder, updateOrderStatus, calculateTotalRevenue, deleteOrder };
