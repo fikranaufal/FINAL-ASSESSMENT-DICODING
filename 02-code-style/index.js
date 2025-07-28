fab1fc9e1c68f462ff0e19f42ab94f5525a37b6f
fikranaufal <fikran.naufal@gmail.com>
2025-07-28 14:32:15 +0700

    final 2.0

--- 02-code-style/index.js
+++ 02-code-style/index.js
@@ -19,8 +19,11 @@ function saveBook(book) {
 };
 
 saveBook({ id: 'book-1', name: 'Book 1' });
+
 const myBooks = getBooks();
+
 const myBook = getBookById('book-1');
 
 console.log(myBooks);
+
 console.log(myBook);
